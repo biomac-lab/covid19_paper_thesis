6fae068806d4d6c7610cb83e93a94fb3b28a740e
Jaime Cascante <je.cascante10@uniandes.edu.co>
2021-02-08 09:59:12 -0500

    cleaning code

--- inference_BOG_SEIR_deaths.m
+++ inference_BOG_SEIR_deaths.m
@@ -1,6 +1,3 @@
-addpath(genpath('./npy-matlab/npy-matlab'))
-addpath(genpath('./SEIR_deaths'))
-
 %Inference for the metapopulation SEIR model
 clear all, clc
 
@@ -142,7 +139,7 @@ for n=1:Iter
             display(strcat('Number of cases in t=', num2str(t),' is=',num2str(tot_cases_t)))
             display(strcat('Number of deaths in t=', num2str(t),' is=',num2str(tot_cases_h)))
         end
-        
+
         %add reporting delay
         for k=1:num_ens
             for l=1:num_loc
@@ -156,13 +153,12 @@ for n=1:Iter
                 end
             end
         end
-        
+
         obs_temp_H(:,:,t) = obs_cnt_H; % No delay to deaths
-        obs_ens   = obs_temp(:,:,t);%observation at t
-        obs_ens_H = obs_temp_H(:,:,t);%observation at t
-        
-        x_prior(:,:,t)=x;%set prior
-        
+        obs_ens   = obs_temp(:,:,t); % observation at t
+        obs_ens_H = obs_temp_H(:,:,t); % observation at t
+        x_prior(:,:,t)=x; % set prior
+
         % AJUST USING DEATHS
         % loop through local observations
         %loop through local observations
@@ -186,27 +182,26 @@ for n=1:Iter
             neighbors = [neighbors;l];%add location l
             for i=1:length(neighbors)
                 idx=neighbors(i);
-
                 for j=1:7
                     A=cov( x((idx-1)*7+j,:),obs_ens_H(l,:));
                     rr((idx-1)*7+j)=A(2,1)/prior_var;
                 end
             end
-            
+
             for i=num_loc*7+1:num_loc*7+8
                 A=cov(x(i,:),obs_ens_H(l,:));
                 rr(i)=A(2,1)/prior_var;
             end
-            
+
             %Get the adjusted variable
             dx = rr'*dy;
             x  = x+dx;
             %Corrections to DA produced aphysicalities
             x = checkbound_SEIR_deaths(x,pop);
-            
+
         end
-                
-        
+
+
         % AJUST USING OBSERVATIONS
         % loop through local observations
         %loop through local observations
@@ -228,7 +223,7 @@ for n=1:Iter
             rr=zeros(1,num_var);
             neighbors = union(find(sum(M(:,l,:),3)>0),find(sum(M(l,:,:),3)>0));
             neighbors = [neighbors;l];%add location l
-            
+
             for i=1:length(neighbors)
                 idx=neighbors(i);
                 for j=1:7
@@ -236,58 +231,31 @@ for n=1:Iter
                     rr((idx-1)*5+j)=A(2,1)/prior_var;
                 end
             end
-            
+
             for i=num_loc*7+1:num_loc*7+8
                 A=cov(x(i,:),obs_ens(l,:));
                 rr(i)=A(2,1)/prior_var;
             end
-            
+
             %Get the adjusted variable
             dx = rr'*dy;
             x  = x+dx;
             %Corrections to DA produced aphysicalities
             x = checkbound_SEIR_deaths(x,pop);
-            
+
         end
-        
+
         x_post(:,:,t)      = x;
         para_post(:,:,t,n) = x(end-7:end,:);
     end
-    
+
     para         = x_post(end-7:end,:,1:num_times);
     temp         = squeeze(mean(para,2)); %average over ensemble members
     theta(:,n+1) = mean(temp,2);  %average over time
-    
-        % loc_mas500 = 1:2;
-        %     if mod(n,100)==0 || n==1
-        %         for special_loc = loc_mas500
-        %             figure
-        %             obs_fitted = obs_temp(special_loc,:,:);
-        %             obs_fitted =  mean((obs_fitted),2);
-        %             
-        %             obs_fitted_h = obs_temp_H(special_loc,:,:);
-        %             obs_fitted_h =  mean((obs_fitted_h),2);
-        %             
-        %             subplot(1,2,1)
-        %             plot(1:num_times, squeeze(obs_fitted),'k-','LineWidth',2)
-        %             hold on
-        %             plot(1:num_times, obs_truth(special_loc,:),'rx')
-        %             legend('Fit','Data')
-        %             hold off
-        %             
-        %             subplot(1,2,2)
-        %             plot(1:num_times, squeeze(obs_fitted_h),'k--','LineWidth',2)
-        %             hold on
-        %             plot(1:num_times, obs_truth_hosp(special_loc,:),'bx')
-        %             legend('Fit deaths','Data deaths')
-        %             hold off
-        %             
-        %         end
-        %         drawnow
-        %     end
+
+
     if mod(n,50)==0
         save(strcat( '/Users/chaosdonkey06/Dropbox/EAKF_Forecast/colombia/checkpoints_col/',num2str(n),'_xstates_BOG') , 'x_post');
-        save(strcat( '/Users/chaosdonkey06/Dropbox/EAKF_Forecast/colombia/checkpoints_col/',num2str(n),'_parapost_states_BOG') , 'para_post');
         para_post_mean = mean(para_post(:,:,:,1:n), 4);
         save(strcat( '/Users/chaosdonkey06/Dropbox/EAKF_Forecast/colombia/checkpoints_col/',num2str(n),'_parapost-mean_states_col') , 'para_post_mean');
     end
@@ -297,62 +265,3 @@ end
 %%%%
 t_end = toc;
 print('Time running: '+num2str(t_end-t_init))
-
-%% Define confidence interval function
-
-% x is a vector, matrix, or any numeric array of data. NaNs are ignored.
-% p is the confidence level (ie, 95 for 95% CI)
-% The output is 1x2 vector showing the [lower,upper] interval values.
-CIFcn = @(x) prctile(x,abs([0,100]-(100-95)/2));
-
-%% Calculate confidence intervals
-obsidx=(5:5:5*num_loc)';
-obs_sim = obs_temp; %x_post_sim(obsidx, :,:);
-
-%save('xstates_forecast_col_may22_may20_t_sim_136','x_post_sim');
-obs_sim = sort(obs_sim,2);
-
-ci = zeros(2,num_loc,num_times);
-for loc =1:num_loc
-    for t = 1:num_times
-        ens_data = squeeze(obs_sim(loc,:,t));
-        ci(:,loc,t) = CIFcn(ens_data);
-    end
-end
-
-loc_mas500 = 1:10;
-
-up_ci = squeeze(ci(1,:,:));
-low_ci = squeeze(ci(2,:,:));
-
-%%
-loc_mas500 = 1:5;
-cit = {'Kenedy','2','3', '4', '5', '6', 'VILLAVICENCIO', 'SAN ANDRES DE TUMACO', 'MEDELLIN', 'BUENAVENTURA'};
-
-i=1;
-num_forecast= 0;
-for special_loc = loc_mas500
-    figure
-    obs_sim_i = obs_sim(special_loc,:,:);
-    
-    obs_fitted =  squeeze(median((obs_sim_i),2));
-    plot(1:num_times, obs_fitted(1:num_times),'k-','LineWidth',2)
-    
-    hold on
-    plot(num_times+1:num_times+num_forecast,obs_fitted(num_times+1:num_times+num_forecast),'b-','LineWidth',2)
-    
-    plot(1:num_times, obs_truth(special_loc,:),'ro','LineWidth',2)
-    
-    plot(1:num_times, up_ci(special_loc,1:num_times),'Color', uint8([17 17 17]), 'LineStyle','--','LineWidth',2)
-    plot(1:num_times, low_ci(special_loc,1:num_times),'Color', uint8([17 17 17]), 'LineStyle','--','LineWidth',2)
-    
-    %plot(num_times+1:num_times+num_forecast, up_ci(special_loc,num_times+1:num_times+num_forecast),'Color', 'b', 'LineStyle','--','LineWidth',2)
-    %plot(num_times+1:num_times+num_forecast, low_ci(special_loc,num_times+1:num_times+num_forecast),'Color','b', 'LineStyle','--','LineWidth',2)
-    
-    title(cit{i})
-    legend('Fit','Data')
-    hold off
-    i=i+1;
-    
-end
-
